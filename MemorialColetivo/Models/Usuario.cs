8b41e77c021b336a33623b9a3a09b0f6702396d4
Luis <luisflaviomont@gmail.com>
2023-08-01 22:50:18 -0300

    01/08 - Usuario Model

---
+++ MemorialColetivo/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemorialColetivo.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioID { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
