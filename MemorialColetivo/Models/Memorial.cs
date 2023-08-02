dc50000d7d99c636318df5aaa3d34b457f9156ec
Luis <luisflaviomont@gmail.com>
2023-08-01 23:01:50 -0300

    01/08 - Memorial Model

---
+++ MemorialColetivo/Models/Memorial.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemorialColetivo.Models
+{
+    public class Memorial
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Historia { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataFalecimento { get; set; }
+        public string Formacao { get; set; }
+        public string Religiao { get; set; }
+        public string Hobbies { get; set; }
+
+    }
+}
