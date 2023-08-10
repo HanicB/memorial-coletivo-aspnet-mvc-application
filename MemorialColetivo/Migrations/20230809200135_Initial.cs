96036e48823969905dc38181f318326ed081620e
Luis <luisflaviomont@gmail.com>
2023-08-10 17:32:41 -0300

    10/08 - SQL connection and tables created

---
+++ MemorialColetivo/Migrations/20230809200135_Initial.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemorialColetivo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Memorial",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Historia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataFalecimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Formacao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Religiao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Hobbies = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memorial", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    UsuarioID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProfilePictureURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.UsuarioID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario_Memorial",
+                columns: table => new
+                {
+                    MemorialId = table.Column<int>(type: "int", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario_Memorial", x => new { x.UsuarioId, x.MemorialId });
+                    table.ForeignKey(
+                        name: "FK_Usuario_Memorial_Memorial_MemorialId",
+                        column: x => x.MemorialId,
+                        principalTable: "Memorial",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Usuario_Memorial_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "UsuarioID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_Memorial_MemorialId",
+                table: "Usuario_Memorial",
+                column: "MemorialId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario_Memorial");
+
+            migrationBuilder.DropTable(
+                name: "Memorial");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
