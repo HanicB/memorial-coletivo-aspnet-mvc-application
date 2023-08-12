f334d7edb617e93c7aff6390d8f2ced1282401c6
Luis <luisflaviomont@gmail.com>
2023-08-12 01:51:24 -0300

    12/08/23 - Controllers

---
+++ MemorialColetivo/Controllers/MemorialController.cs
@@ -0,0 +1,21 @@
+﻿using MemorialColetivo.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemorialColetivo.Controllers
+{
+    public class MemorialController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MemorialController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Memorial.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ MemorialColetivo/Controllers/UsuariosController.cs
@@ -0,0 +1,21 @@
+﻿using MemorialColetivo.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace MemorialColetivo.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Usuario.ToList();
+            return View();
+        }
+    }
+}
